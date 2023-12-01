035e8de4a7bcbaefd518e6360b6a27b8e4014b2a
MartaBahrynivska <martabagrynivska@googlemail.com>
2023-11-30 20:45:56 -0500

    gallery-done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = galleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function galleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => `
+     <a class="gallery-item" href="${original}">
+     <img class="gallery-image"
+          src="${preview}" 
+          alt="${description}" />
+     </a>
+    `).join("")
+}
+
+
+
+
+
+
+console.log(galleryItems);
